0f80b62f8091803c4644e0444035aa353bfcd6cc
mickengland <england.mick@gmail.com>
2017-08-18 11:34:06 -0400

    Testing rollback

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "Hello Kubernetes. Let's test a rollback!")
+    io.WriteString(w, "Hello Kubernetes. Version 1")
 }
 
 func main() {
